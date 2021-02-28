f920475853040033b7266ba1cdfe84efbcb39ece
alvaro rodrigues <alvim.lopez@gmail.com>
2021-02-28 09:29:34 -0300

    project criado

---
+++ backend/src/main/java/com/alvarolopes/aldeliver/AldeliverApplication.java
@@ -0,0 +1,13 @@
+package com.alvarolopes.aldeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AldeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AldeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alvarolopes/aldeliver/AldeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvarolopes.aldeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AldeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
